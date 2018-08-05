0c21ebdb89904b30e3d2399cceb4bb047269f3e7
Akky <akshaydp07@gmail.com>
2018-08-05 23:07:25 +0530

    fist commit to get spring boot structure

---
+++ src/main/java/com/jda/preps/SpringBootApplications/SpringBootApplicationsApplication.java
@@ -0,0 +1,12 @@
+package com.jda.preps.SpringBootApplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationsApplication.class, args);
+	}
+}
