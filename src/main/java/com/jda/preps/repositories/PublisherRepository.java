754ad4cf3e54a245442bbff723f1b59c15f005f5
Akky <akshaydp07@gmail.com>
2018-08-05 23:51:09 +0530

    added repositories for curd operations

---
+++ src/main/java/com/jda/preps/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.jda.preps.repositories;
+
+import com.jda.preps.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
